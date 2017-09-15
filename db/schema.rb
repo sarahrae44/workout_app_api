acdc8e0feb1c7dbfcdcd930e3382968bd3af229d
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-09-14 21:59:57 -0600

    set up authorization backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914231919) do
+ActiveRecord::Schema.define(version: 20170915011850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20170914231919) do
     t.datetime "updated_at", null: false
     t.index ["body_id"], name: "index_groups_on_body_id"
     t.index ["exercise_id"], name: "index_groups_on_exercise_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "groups", "bodies"
