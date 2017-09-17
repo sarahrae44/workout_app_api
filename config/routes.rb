c95e422455ac7b74118b91a55a0ea88f9b93f2a3
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-09-17 11:10:56 -0600

    can add an exercise

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :bodies, only: [:index, :show, :update, :destroy, :create]
   # resources :movements
   # resources :bodyparts
-  resources :exercises
+  resources :exercises, only: [:index, :show, :update, :destroy, :create]
   # resources :muscles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
