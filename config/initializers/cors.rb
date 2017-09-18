48d8b950e1dea04e993e15c56c3489a6669246f3
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-09-17 19:36:13 -0600

    heroku troubleshooting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,19 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4040'
+    origins ['workout-frontend.herokuapp.com', 'localhost:4040']
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :options, :head]
+  end
+
 end
