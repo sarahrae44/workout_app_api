f2d1dbfc19a3d7f01545f3de7c4c1cf2a1d45473
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-09-18 17:23:39 -0600

    heroku fix

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   # attr_accessible :body_id, :exercise_id
   belongs_to :body
   belongs_to :exercise
-  validates :body_id, presence: true
-  validates :exercise_id, presence: true
+  # validates :body_id, presence: true
+  # validates :exercise_id, presence: true
   # accepts_nested_attributes_for :body, :exercise
 end
