73a2b7062e93d4601d879a2492de52038f43be27
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-09-19 08:48:31 -0600

    updated CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  ['*', 'workout-frontend.herokuapp.com', 'localhost:4040']
+    origins  ['workout-frontend.herokuapp.com', 'localhost:4040']
 
     resource '*',
       headers: :any,
